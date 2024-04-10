69784d893a92df408d0292c96b2706feacae9865
gazzsha <theexorc1st@yandex.ru>
2024-04-10 16:24:14 +0300

    #1 Added README.md, checkstyle, actions

---
+++ src/main/java/gazzsha/com/tasklist/repository/jpa/TaskRepository.java
@@ -0,0 +1,20 @@
+package gazzsha.com.tasklist.repository.jpa;
+
+import gazzsha.com.tasklist.domain.task.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {
+    @Query(value = """
+            select * from tasks t
+            join users_tasks ut on ut.task_id = t.id
+            where ut.user_id =: userId
+            """, nativeQuery = true)
+    List<Task> findAllByUserId(@Param("userId") Long userId);
+}
---
+++ src/main/java/gazzsha/com/tasklist/repository/jpa/UserRepository.java
@@ -0,0 +1,24 @@
+package gazzsha.com.tasklist.repository.jpa;
+
+import gazzsha.com.tasklist.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findAllByUsername(String username);
+
+    @Query(value = """
+                  SELECT EXISTS(SELECT 1
+                          FROM users_tasks
+                          WHERE user_id = :userId
+                            AND task_id = :taskId)
+            """, nativeQuery = true)
+    boolean isTaskOwner(@Param("userId") Long userId, @Param("taskId") Long taskId);
+}
